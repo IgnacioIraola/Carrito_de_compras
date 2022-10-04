3f06ad88ce7086059b6545b228d143036670d757
IgnacioIraola <iraolai@hotmail.com>
2022-10-04 20:17:40 -0300

    Creado

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// función registro
+
+function signup(e){
+event.preventDefault();
+
+
+let email = document.getElementById("email").value;
+let username = document.getElementById("username").value;
+let pass = document.getElementById("password").value;
+let result = document.getElementById("result");
+
+let user = {
+    email: email,
+    username: username,
+    password: pass,
+};
+
+if (username === "" || email === "" || password ==="") {
+    result.innerHTML = "*Todos los campos son obligatorios";
+    return false;
+}else{
+    alert("Bienvenido ingresá al link de abajo");
+
+}
+
+
+
+let json = JSON.stringify(user);
+localStorage.setItem(username, json);
+
+
+}
+
+// función login
+
+function loginFunc(e) {
+    event.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+    let result = document.getElementById("result");
+    
+    let user = localStorage.getItem(username);
+    let data =JSON.parse(user);   
+    console.log(data);
+
+    if (user == null) {
+        result.innerHTML = "*Usuario erróneo";
+    } else if(username == data.username && pass == data.password){
+         alert("Bienvenido!")
+        
+        window.location.href = "index.html"
+    }else{
+        result.innerHTML = "*Password erróneo";
+    }
+}
